c5042e6e86394aef2702ae6ce14ea557f30d530d
Rajiv-test <tathireddyrajivreddy@gmail.com>
2025-01-05 15:22:26 +0530

    sixth commit: added entrance,exit and tests

--- maze.py
+++ maze.py
@@ -1,8 +1,20 @@
 from cell import Cell
-from time import sleep
+import random
+import time
+
 
 class Maze:
-    def __init__(self,x1,y1,num_rows,num_cols,cell_size_x,cell_size_y,win):
+    def __init__(
+        self,
+        x1,
+        y1,
+        num_rows,
+        num_cols,
+        cell_size_x,
+        cell_size_y,
+        win=None,
+    ):
+        self._cells = []
         self._x1 = x1
         self._y1 = y1
         self._num_rows = num_rows
@@ -10,49 +22,38 @@ class Maze:
         self._cell_size_x = cell_size_x
         self._cell_size_y = cell_size_y
         self._win = win
-        
+
         self._create_cells()
-    
+        self._break_entrance_and_exit()
+
     def _create_cells(self):
-        self._cells = [[]for row in range(self._num_rows)]
-        for row in range(len(self._cells)):
-            for col in range(self._num_cols):
-                self._cells[row].append(Cell(self._win))
-        
-        for row in range(len(self._cells)):
-            for col in range(self._num_cols):
-                self._draw_cell(self._cells[row][col],row,col)
-        
-    def _draw_cell(self,cell,i,j):
-        x1 = self._x1+(self._cell_size_x * i)
-        y1 = self._y1+(self._cell_size_y * j)
+        for i in range(self._num_cols):
+            col_cells = []
+            for j in range(self._num_rows):
+                col_cells.append(Cell(self._win))
+            self._cells.append(col_cells)
+        for i in range(self._num_cols):
+            for j in range(self._num_rows):
+                self._draw_cell(i, j)
+
+    def _draw_cell(self, i, j):
+        if self._win is None:
+            return
+        x1 = self._x1 + i * self._cell_size_x
+        y1 = self._y1 + j * self._cell_size_y
         x2 = x1 + self._cell_size_x
         y2 = y1 + self._cell_size_y
-        cell.draw(x1,y1,x2,y2)
+        self._cells[i][j].draw(x1, y1, x2, y2)
         self._animate()
-       
-       
+
     def _animate(self):
         if self._win is None:
-            return None
+            return
         self._win.redraw()
-        sleep(0.005)
-       
-       
-       
-       
-       
-       
+        time.sleep(0.05)
 
-       
-       
-       
-       
-                 
-                
-
-
-
-
-        
-    
+    def _break_entrance_and_exit(self):
+        self._cells[0][0].has_top_wall = False
+        self._draw_cell(0, 0)
+        self._cells[self._num_cols - 1][self._num_rows - 1].has_bottom_wall = False
+        self._draw_cell(self._num_cols - 1, self._num_rows - 1)
